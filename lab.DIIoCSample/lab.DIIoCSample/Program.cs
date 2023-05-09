9fefba59803a09610d6b8c4687d0a763457371d9
Rasel Ahmmed <raselahmmed@hotmail.com>
2023-05-09 11:23:35 +0600

    first

---
+++ lab.DIIoCSample/lab.DIIoCSample/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-Dependency Injection (DI) & Inversion of Control (IoC)-");
+Console.WriteLine("--------------------------------------------------------");
+Console.WriteLine("--------------------------------------------------------");
+
+
+Console.WriteLine("---------------------------End--------------------------");
+Console.ReadLine();
