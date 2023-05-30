79cc51e3d26223fca8ef4d18c8aabdd3b5911c28
Rasel Ahmmed <raselahmmed@hotmail.com>
2023-05-30 16:10:37 +0600

    2023-05-30

---
+++ lab.DIIoCSample/lab.DIIoCSample/DatabaseContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab.DIIoCSample
+{
+    public class DatabaseContext
+    {
+        //private MSSql _db;
+        //private Oracle _db;
+        private readonly IConnect _db;
+
+        public DatabaseContext(IConnect db)
+        {
+            _db = db;
+        }
+
+        public void ConnectDatabase()
+        {
+            _db.Connect();
+        }
+    }
+
+    public interface IConnect
+    {
+        public void Connect();
+    }
+
+    public class MSSql : IConnect
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Connect to MS SQL");
+        }
+    }
+
+    public class Oracle : IConnect
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Connect to Oracle");
+        }
+    }
+}
--- lab.DIIoCSample/lab.DIIoCSample/Program.cs
+++ lab.DIIoCSample/lab.DIIoCSample/Program.cs
@@ -1,7 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using lab.DIIoCSample;
+
 Console.WriteLine("-Dependency Injection (DI) & Inversion of Control (IoC)-");
 Console.WriteLine("--------------------------------------------------------");
 Console.WriteLine("--------------------------------------------------------");
+
+MSSql msSql = new MSSql();
+//DatabaseContext databaseContext = new DatabaseContext(msSql);
+Oracle oracle = new Oracle();
+DatabaseContext databaseContext = new DatabaseContext(msSql);
+databaseContext.ConnectDatabase();
 
 
 Console.WriteLine("---------------------------End--------------------------");
